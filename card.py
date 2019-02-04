68ef5a78adc2f7c34b57422e35f78c225c2915e3
Nandana <nandana.srinivasan@dal.ca>
2019-02-04 11:28:33 -0400

    Secong commit

--- card.py
+++ card.py
@@ -9,5 +9,17 @@ class card():
 	def __str__(self):
 		return f"The rank is {card.rank_names[self.rank]} and the suit is {card.suit_names[self.suit]}"
 
+	def __lt__(self,other):
+		return self.rank < other.rank
+
+	def __gt__(self,other):
+		return self.rank>other.rank
+
+	def __et__(self,other):
+		return self.rank==other.rank
+
 ace_spade = card(3,1)
+queen_hearts = card(2,12)
 print(ace_spade)
+print(queen_heart)
+
